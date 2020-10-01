b80a05aca61e6e7423db2e39078f8ab5d3db7dd0
Tyler Jacobson <TylerJacobsonSE@gmail.com>
2020-10-01 16:55:40 -0700

    MVP Met FUCK THIS SHIT

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,31 +1,37 @@
 import React from "react";
-import { fireEvent, render, screen } from "@testing-library/react";
-import ContactForm from './ContactForm'
+import { act, fireEvent, render, screen } from "@testing-library/react";
+import ContactForm from "./ContactForm";
 
 test("renders ContactForm without crashing", () => {
+  render(<ContactForm />);
+});
+
+test("Attempts to validate form fields", async () => {
+    // Arrange
     render(<ContactForm />);
-  });
-  
-  test('User can fill out and submit ContactForm',  () => {
-    //Arrange
-      //render our component
-    render(<ContactForm />);
-  
-    //Act
-      //query each input
-      //fill out our form elements
-      //click our button
-    const firstNameInput = screen.queryByLabelText("First Name*")
-    // const lastNameInput = screen.getByLabelText('Last Name*')
-    // const emailInput = screen.getByLabelText('Email*')
-    const button = screen.getByRole("button", {name:/submit/i});
-  
-    fireEvent.change(firstNameInput, {target:{name:'firstName', value:"tim"}})
-  
-    fireEvent.click(button)
-  
-    //Assert
-      //test that our form input exists on the page
-  
-  
-  })
+
+    // Act
+    await act(async () => {
+    const firstNameInput = screen.getByLabelText(/first name/i)
+    const lastNameInput = screen.getByLabelText(/Last name/i)
+    const emailInput = screen.getByLabelText(/email/i)
+    const messageInput = screen.getByLabelText(/message/i)
+    const buttonInput = await screen.getByRole(/button/i, {name:/submit/i})
+
+    fireEvent.change(firstNameInput, {target:{name:"firstName", value:"timmy"}})
+    expect(firstNameInput.value === "timmy").toBeTruthy()
+
+    fireEvent.change(lastNameInput, {target:{name:"lastName", value:"timmyson"}})
+    expect(lastNameInput.value === "timmyson").toBeTruthy()
+
+    fireEvent.change(emailInput, {target:{name:"email", value:"timmy@timmy.com"}})
+    expect(emailInput.value === "timmy@timmy.com").toBeTruthy()
+
+    fireEvent.change(messageInput, {target:{name:"message", value:"timmy"}})
+    expect(messageInput.value === "timmy").toBeTruthy()
+
+    expect(buttonInput).toBeInTheDocument()
+    fireEvent.click(buttonInput)
+    })
+    // Assert
+});
